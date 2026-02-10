d3d07c362d85bff2b8bc2cf6cfaea342e89b6f37
Andy Vandenberghe <andreasvdberghe@gmail.com>
2026-02-10 10:54:58 +0100

    Refactor badge presets into dedicated config file
    created a dedicated PHP config file for presets, to load them via event listener, and render them dynamically in template using a Twig loop. No database or ACP page needed

---
+++ config/badge_presets.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package phpBB Extension - Badge
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+return [
+	['key' => 'changelog', 'value' => 'changelog-v1.0.0-a808f3', 'title' => 'Changelog'],
+	['key' => 'custom',    'value' => 'SUBJECT-STATUS-COLOR',     'title' => 'Custom'],
+	['key' => 'license',   'value' => 'license-GPLv2-blue',       'title' => 'License GPLv2'],
+	['key' => 'v100',      'value' => 'version-1.0.0-FFFF00',     'title' => 'Version 1.0.0'],
+	['key' => 'v101',      'value' => 'version-1.0.1-f36808',     'title' => 'Version 1.0.1'],
+	['key' => 'v200',      'value' => 'version-2.0.0-ff5733',     'title' => 'Version 2.0.0'],
+	['key' => 'v300',      'value' => 'version-3.0.0-ff5733',     'title' => 'Version 3.0.0'],
+	['key' => 'v400',      'value' => 'version-4.0.0-ff5733',     'title' => 'Version 4.0.0'],
+];
--- event/listener.php
+++ event/listener.php
@@ -18,10 +18,24 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/** @var string */
+	protected $ext_path;
+
+	/**
+	* Constructor
+	*
+	* @param string $ext_path Path to this extension's directory
+	*/
+	public function __construct($ext_path)
+	{
+		$this->ext_path = $ext_path;
+	}
+
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.user_setup'	=> 'load_language_on_setup',
+			'core.user_setup'						=> 'load_language_on_setup',
+			'core.posting_modify_template_vars'		=> 'load_badge_presets',
 		);
 	}
 
@@ -33,5 +47,20 @@ class listener implements EventSubscriberInterface
 			'lang_set' => 'common',
 		);
 		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	public function load_badge_presets($event)
+	{
+		$presets_file = $this->ext_path . 'config/badge_presets.php';
+		$presets = [];
+
+		if (file_exists($presets_file))
+		{
+			$presets = include $presets_file;
+		}
+
+		$page_data = $event['page_data'];
+		$page_data['badge_presets'] = $presets;
+		$event['page_data'] = $page_data;
 	}
 }
